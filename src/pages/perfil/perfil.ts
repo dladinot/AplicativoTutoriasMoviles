8ba1bed4750c344ff501d1f6487f12f4bf6f1888
dladinot <dladinot>
2017-09-17 19:24:45 -0500

    Trabajo de nicolas y jose

--- src/pages/perfil/perfil.ts
+++ src/pages/perfil/perfil.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the PerfilPage page.
@@ -15,11 +16,97 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PerfilPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  testCheckboxOpen: boolean;
+  testCheckboxResult;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PerfilPage');
+  }
+
+  showCheckbox() {
+    let alert = this.alertCtrl.create();
+    alert.setTitle('¿En que asignaturas puede ofrecer asesoría?');
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Matemática',
+      value: 'value1',
+      checked: true
+    });
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Linguística',
+      value: 'value2'
+    });
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Informática',
+      value: 'value3'
+    });
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Ingles',
+      value: 'value4'
+    });
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Física',
+      value: 'value5'
+    });
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Geometría',
+      value: 'value6'
+    });
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Geografía',
+      value: 'value7'
+    });
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Historia',
+      value: 'value8'
+    });
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Química',
+      value: 'value9'
+    });
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Biología',
+      value: 'value10'
+    });
+
+    alert.addInput({
+      type: 'checkbox',
+      label: 'Economía',
+      value: 'value11'
+    });
+
+    alert.addButton('Cancelar');
+    alert.addButton({
+      text: 'Confirmar',
+      handler: data => {
+        console.log('Checkbox data:', data);
+        this.testCheckboxOpen = false;
+        this.testCheckboxResult = data;
+      }
+    });
+    alert.present();
   }
 
 }
